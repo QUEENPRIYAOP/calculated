42844e9b6a4f6c343274294879f65d181b4a93a4
QUEENPRIYAOP <95538576+QUEENPRIYAOP@users.noreply.github.com>
2022-03-10 11:35:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-Import os
+#  Copyright (c) 2021 thanosuser
+
+import logging
+import re
+
+from pyrogram import Client
+from pyrogram.errors import (
+    FloodWait,
+    PhoneCodeExpired,
+    PhoneCodeInvalid,
+    PhoneNumberInvalid,
+    SessionPasswordNeeded,
+)
+from telethon import Button, TelegramClient, events
+from telethon.errors.rpcerrorlist import FloodWaitError as fwe
+from telethon.errors.rpcerrorlist import PasswordHashInvalidError as phi
+from telethon.errors.rpcerrorlist import PhoneCodeExpiredError as pce
+from telethon.errors.rpcerrorlist import PhoneCodeInvalidError as pci
+from telethon.errors.rpcerrorlist import SessionPasswordNeededError as spn
+from telethon.sessions import StringSession
 
 from strings import *
 from var import Var
 
-
-
-
-
+logging.basicConfig(level=logging.INFO)
 
 bot = TelegramClient(None, Var.API_ID, Var.API_HASH).start(bot_token=Var.BOT_TOKEN)
 
