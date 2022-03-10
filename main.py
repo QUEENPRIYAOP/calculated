3446178cc960430a31a90d466469484f64b2f4ff
QUEENPRIYAOP <95538576+QUEENPRIYAOP@users.noreply.github.com>
2022-03-10 11:49:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ bot = TelegramClient(None, Var.API_ID, Var.API_HASH).start(bot_token=Var.BOT_TOK
 
 @bot.on(events.NewMessage(pattern="/start", incoming=True, func=lambda e: e.is_private))
 
-a = int(input('Enter 1st number: '))
-b = int(input('Enter 2nd number: '))
+a = input('Enter 1st number: '))
+b = input('Enter 2nd number: '))
 a = int(a)
 b = int(b)
 ab = (a+b)
